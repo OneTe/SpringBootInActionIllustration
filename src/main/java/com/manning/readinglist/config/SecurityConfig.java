2a3d7179d71c568a8bee35869cd9c57ab121ce1a
wangcheng <veoxft@hotmail.com>
2018-06-23 22:20:26 +0800

    Add Profile annotation and change injection style

--- src/main/java/com/manning/readinglist/config/SecurityConfig.java
+++ src/main/java/com/manning/readinglist/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.manning.readinglist.config;
 
 import com.manning.readinglist.repository.ReaderRepository;
+import com.manning.readinglist.repository.ReadingListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,12 +16,17 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 /**
  * Created by wangcheng  on 2018/6/22.
  */
+@Profile("development")//这里要求运行时激活development Profile，这样才能应用该配置，设置spring.profiles.active的属性就能激活，见application.yml配置
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
+
     private ReaderRepository readerRepository;
 
+    @Autowired
+    public SecurityConfig(ReaderRepository readerRepository){
+        this.readerRepository = readerRepository;
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
